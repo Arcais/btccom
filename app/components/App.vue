99e429981065291049fe7120926a02e2a5fc5df2
arcais <xclubcontest@gmail.com>
2018-06-12 02:30:35 +0200

    Basic functionality -HTTP Get action -Live listener for changes with state keeping -UI Kit with tables that contain filtered elements

--- app/components/App.vue
+++ app/components/App.vue
@@ -1,26 +1,98 @@
 <template>
 
-  <button @click="getTransactions" class="uk-button uk-button-default">Test Button</button>
+  <div>
+
+    <!-- <button @click="loadOrders('sell',1,20)" class="uk-button uk-button-default">Test Button</button> -->
+    
+    <table class="uk-table uk-table-hover">
+      
+      <caption>Sell Orders</caption>
+      
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Type</th>
+          <th>Quantity</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="order in filterByLastNElements(sellOrders,20)">
+          <td>{{order.id}}</td>
+          <td>{{order.type}}</td>
+          <td>{{order.quantity}}</td>
+          <td>{{order.price}}</td>
+        </tr>
+      </tbody>
+
+    </table>
+    
+    <table class="uk-table uk-table-hover">
+      
+      <caption>Buy Orders</caption>
+      
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Type</th>
+          <th>Quantity</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="order in filterByLastNElements(buyOrders,20)">
+          <td>{{order.id}}</td>
+          <td>{{order.type}}</td>
+          <td>{{order.quantity}}</td>
+          <td>{{order.price}}</td>
+        </tr>
+      </tbody>
+
+    </table>
+
+  </div>
 
 </template>
 
 <script>
 // import List from './List.vue';
-import {mapState} from 'vuex';
+import {mapState, mapActions} from 'vuex';
 
 export default {
   name: 'app',
   data() {
     return {
+      idList: [0,100]
+      // prevItemCount: 0
     };
   },
   computed: {
-    // ...mapState(['transactions'])
+    ...mapState(['orders','sellOrders','buyOrders'])
   },
   methods: {
-    getTransactions() {
-      console.log('stuff');
+    ...mapActions(['loadOrders']),
+    filterByLastNElements: function(list, n){
+      return list.slice(Math.max(list.length - n, 1)).reverse();
     }
+  },
+  created: function () {
+    setInterval(function () {
+
+      if(this.orders[this.orders.length-1]){
+
+        this.idList[0]=this.orders[this.orders.length-1].id;
+        this.idList[1]=this.orders[this.orders.length-1].id+100;
+
+      }
+
+      this.loadOrders(this.idList);
+
+      // console.log(this.idList[0]-this.prevItemCount);
+      // this.prevItemCount = this.idList[0];
+
+    }.bind(this), 1000); 
   },
   components: {
     // List
--- app/store/index.js
+++ app/store/index.js
@@ -1,15 +1,38 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VueResource from 'vue-resource';
 
 Vue.use(Vuex);
+Vue.use(VueResource);
 
 export default new Vuex.Store({
   state: {
-    transactions: []
+    orders: [],
+    sellOrders: [],
+    buyOrders: []
+  },
+  actions:{
+    loadOrders (context, idList){
+
+      context.commit('loadOrders', idList);
+    }
   },
   mutations: {
-    loadTransaction(type) {
-      //...
+    loadOrders (state, idList) {
+
+      Vue.http.get(`http://localhost:5001/listOrders?start=${idList[0]}&size=${idList[1]}`).then(res => {
+
+        state.orders = state.orders.concat(res.body);
+
+        state.sellOrders = state.orders.filter( order => order.type==='sell' );
+        state.buyOrders = state.orders.filter( order => order.type==='buy' );
+
+      }, err => {
+
+        console.error(err);
+
+      });
+
     }
   }
 });
